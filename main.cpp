9a30927bcd957e4d446947f9eaee8bef28457010
BUIHUUHAIDANG <dangbui0408@gmail.com>
2025-05-10 11:25:30 +0700

    lan dau tien commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int x;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>x;
+cout<<(x*(x+1))/2<<endl;
+    }
+
+    return 0;
+}
